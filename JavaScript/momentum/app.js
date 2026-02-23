558957cd168c5131ceb93eb259c7de9bcc16b1da
Minhyuk03 <mcjmh1103@gmail.com>
2026-02-23 15:36:01 +0900

    2026-02-23 js

--- JavaScript/momentum/app.js
+++ JavaScript/momentum/app.js
@@ -1,5 +1,32 @@
-const title = document.getElementById("title");
+const h1 = document.querySelector(".hello:first-child h1");
 
-console.dir(title);
+function handleTitleClick(){
+    h1.style.color = "blue";
+}
+function handleMouseEnter(){
+   h1.innerText = "Mouse is here!";
+}
+function handleMouseLeave(){
+    h1.innerText = "Mouse is gone!";
+}
+function handleWindowResize(){
+    document.body.style.backgroundColor = "tomato";
+}
+function handleWindowCopy(){
+    alert("copier!");
+}   
+function handleWindowOffline(){
+    alert("SOS no WIFI");
+}
+function handleWindowOnline(){
+    alert("ALL GOOD");
+}
+h1.addEventListener("click", handleTitleClick);
+h1.addEventListener("mouseenter", handleMouseEnter);
+h1.addEventListener("mouseleave", handleMouseLeave);
 
-title.innerText = "Got you!";
+window.addEventListener("resize", handleWindowResize);
+window.addEventListener("copy", handleWindowCopy)
+window.addEventListener("offline", handleWindowOffline);
+window.addEventListener("online", handleWindowOnline);
+
